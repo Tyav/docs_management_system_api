e5647eca7e4645300e4e0c2048f3dae953fd5c45
Tyav <tyav2greenz@gmail.com>
2019-04-09 19:58:05 +0100

    Create User: wrote test for username validation with Joi

---
+++ test/unit/user/validate-user.test.js
@@ -0,0 +1,71 @@
+import 'babel-polyfill';
+import { validateCreateUser, validateLogin } from '../../../server/validations/user';
+
+describe('Validation of new user creation details', () => {
+	describe('test cases for username', () => {
+		it('should return error if username is not given', () => {
+			let testData = {
+				name: {
+					firstName: 'TestData',
+					lastName: 'TestData',
+				},
+				email: 'testdata@test.data',
+				password: '12345678',
+				roleId: 'dfe5343fef4e3f3r4',
+			};
+			let { error } = validateCreateUser(testData);
+			expect(error).not.toBeNull();
+    });
+    it('should return error if username is less than 3', () => {
+			let testData = {
+				username: 'Te',
+				name: {
+					firstName: 'TestData',
+					lastName: 'TestData',
+				},
+				email: 'testdata@test.data',
+				password: '12345678',
+				roleId: 'dfe5343fef4e3f3r4',
+			};
+			let { error } = validateCreateUser(testData);
+			expect(error).not.toBeNull();
+		});
+    it('should return error if username is not a string', () => {
+			let testData = {
+				username: 43543,
+				name: {
+					firstName: 'TestData',
+					lastName: 'TestData',
+				},
+				email: 'testdata@test.data',
+				password: '12345678',
+				roleId: 'dfe5343fef4e3f3r4',
+			};
+			let { error } = validateCreateUser(testData);
+			expect(error).not.toBeNull();
+		});
+  });
+  describe('test case for Name', () => {
+    it('should return error if Name is not provided', () => {
+			let testData = {
+				username: 'TestData',
+				email: 'testdata@test.data',
+				password: '12345678',
+				roleId: 'dfe5343fef4e3f3r4',
+			};
+			let { error } = validateCreateUser(testData);
+			expect(error).not.toBeNull();
+		});
+  });
+});
+
+// let testData = {
+// 	username: 'TestData',
+// 	name: {
+// 		firstName: 'TestData',
+// 		lastName: 'TestData',
+// 	},
+// 	email: 'testdata@test.data',
+// 	password: '12345678',
+// 	roleId: 'dfe5343fef4e3f3r4',
+// };
