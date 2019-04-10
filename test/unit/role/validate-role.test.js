239b7c48d980c8af18abc56ad14df77f6d012215
Tyav <tyav2greenz@gmail.com>
2019-04-10 04:30:55 +0100

    pass test for role input joi validator

--- test/unit/role/validate-role.test.js
+++ test/unit/role/validate-role.test.js
@@ -1,26 +1,25 @@
-import validateRole from '../../../server/validations/role';
+import { validateRole } from '../../../server/validations/role';
 
 describe('Joi Validator of Role', () => {
-  describe('test cases for role title validator', () => {
-    it('should return an error if role title is not provided', () => {
-      let TestData = {
-      }
-      const {testError} = validateRole(TestData)
-      expect(testError).not.toBeNull();
-    });
-    it('should return error if role title is less than three characters', () => {
-      let TestData = {
-        title: "te"
-      }
-      const {testError} = validateRole(TestData)
-      expect(testError).not.toBeNull();
-    });
-    it('should return an error of null value if all conditions are met', () => {
-      let TestData = {
-        title: "testRole"
-      }
-      const {testError} = validateRole(TestData)
-      expect(testError).toBeNull();
-    });
-  });
+	describe('test cases for role title validator', () => {
+		it('should return an error if role title is not provided', () => {
+			let TestData = {};
+			const { error } = validateRole(TestData);
+			expect(error).not.toBeNull();
+		});
+		it('should return error if role title is less than three characters', () => {
+			let TestData = {
+				title: 'te',
+			};
+			const { error } = validateRole(TestData);
+			expect(error).not.toBeNull();
+		});
+		it('should return an error of null value if all conditions are met', () => {
+			let TestData = {
+				title: 'testRole',
+			};
+			const { error } = validateRole(TestData);
+			expect(error).toBeNull();
+		});
+	});
 });
