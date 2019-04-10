ae48266bf33b46893dad9e170b6ef94ca34b845b
Tyav <tyav2greenz@gmail.com>
2019-04-10 20:23:35 +0100

    Passed mongoose schema validation

---
+++ server/model/category.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+//Create Role schema to build role model
+const cateSchema = new Schema({
+	title: {
+		//category title
+		type: String,
+		required: true,
+		minlength: 3,
+		unique: true,
+		lowercase: true,
+	},
+});
+
+const Category = mongoose.model('category', cateSchema);
+
+export { Category };
--- test/unit/category/model.test.js
+++ test/unit/category/model.test.js
@@ -1,4 +1,4 @@
-//import 'babel-polyfill'
+import 'babel-polyfill'
 import { Category } from "../../../server/model/category";
 
 describe('Test for Category model', () => {
