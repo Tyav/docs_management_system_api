9a0b35b9ffd7bacbad7cd83be0448384aff04a0f
Tyav <tyav2greenz@gmail.com>
2019-04-10 13:44:20 +0100

    created test for mongoose schema validation

---
+++ test/unit/document/model.test.js
@@ -0,0 +1,27 @@
+import 'babel-polyfill';
+import { Document } from '../../../server/model/document';
+
+describe('Test for Role model', () => {
+	describe('Creation Test', () => {
+		it('should pass validation if all required infomation is supplied', async () => {
+			let testDoc = new Document({
+				title: 'Fiction',
+				content: 'Some test content',
+				creatorId: new mongoose.Types.ObjectId().toHexString(),
+				access: 'public',
+				categoryId: new mongoose.Types.ObjectId().toHexString(),
+			});
+			expect(testDoc.validateSync()).not.toBeDefined();
+		});
+		it('should fail validation if required infomation is not supplied', async () => {
+			let testDoc = new Document({
+				//title is not supplied
+				content: 'Some test content',
+				creatorId: new mongoose.Types.ObjectId().toHexString(),
+				access: 'public',
+				categoryId: new mongoose.Types.ObjectId().toHexString(),
+			});
+			expect(testDoc.validateSync()).toBeDefined();
+		});
+	});
+});
