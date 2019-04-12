b841ca2db819aa40c4b3a01fc4874a683e7d400a
Tyav <tyav2greenz@gmail.com>
2019-04-12 23:25:22 +0100

    passed simple test for creating documents

--- api/v1/routes/document.js
+++ api/v1/routes/document.js
@@ -2,12 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 import _ from 'lodash';
 import bcrypt from 'bcrypt';
+import { Document } from '../../../server/model/document';
 
 const router = express.Router();
 
-
-
 //POST: CREATE DOCUMENT
+router.post('/', async (req, res) => {
+  //CREATE DOCUMENT
+  const doc = new Document(req.body);
+  await doc.save();
+  res.send()
+});
 
 //GET: GET ALL DOCUMENT
 
