2c46d6d9a12e3d70be45c13e5bc1585d0da2396a
Tyav <tyav2greenz@gmail.com>
2019-04-13 00:21:56 +0100

    passed test for document validation

--- api/v1/routes/document.js
+++ api/v1/routes/document.js
@@ -3,12 +3,21 @@ import mongoose from 'mongoose';
 import _ from 'lodash';
 import bcrypt from 'bcrypt';
 import { Document } from '../../../server/model/document';
+import {validateDoc} from '../../../server/validations/document'
+import adminAuth from '../utils/admin';
+import tokenAuth from '../utils/auth';
+import loginAuth from '../utils/isLogin';
+import ifLogin from '../utils/ifLogin';
+
 
 const router = express.Router();
 
 //POST: CREATE DOCUMENT
-router.post('/', async (req, res) => {
+router.post('/',[tokenAuth, loginAuth], async (req, res) => {
   //CREATE DOCUMENT
+  const {error} = validateDoc(req.body)
+  if (error)return res.status(400).send({ Error: 'Bad Request', message: error.details[0].message });
+
   const doc = new Document(req.body);
   await doc.save();
   res.status(200).send(_.pick(doc,['title','content','creatorId']))
