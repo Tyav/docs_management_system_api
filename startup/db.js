08613b62217a2b2b4f487026cd1e9184973c5fc2
Tyav <tyav2greenz@gmail.com>
2019-04-15 07:37:27 +0100

    refactored code

--- startup/db.js
+++ startup/db.js
@@ -1,17 +1,13 @@
 import 'dotenv/config';
+import config from 'config';
 
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const database = process.env.DATABASE;
 
-module.exports = (val) => {
-	const uris = 'mongodb://localhost/docs-test';
-	if (process.env.NODE_ENV !== 'test') {
-		uris = 'mongodb://localhost/DMSapp';
-	}
-
+module.exports = val => {
 	//const uris = `mongodb+srv://${username}:${password}@dmsdb-ht5wo.mongodb.net/${database}`;
-	val.connect(uris, { useNewUrlParser: true }, () => {
+	val.connect(config.get('database'), { useNewUrlParser: true }, () => {
 		console.log('connected to online dbAtlas...');
 	});
 };
