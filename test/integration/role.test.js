773a9e066848e80dc8eb7ddd88381b956d5b1ceb
Tyav <tyav2greenz@gmail.com>
2019-04-15 11:49:29 +0100

    test for admin authentication before role creation

--- test/integration/role.test.js
+++ test/integration/role.test.js
@@ -8,6 +8,32 @@ import { User } from '../../server/model/user';
 import { Role } from '../../server/model/role';
 
 describe('TEST FOR ROLE', () => {
+  const adminUser = new User({
+		username : 'adminUserName',
+		name     : {
+			firstName : 'testFirstName',
+			lastName  : 'testLastName',
+		},
+		email    : 'admin@test.com',
+		password : 'testPassword',
+		roleId   : mongoose.Types.ObjectId(),
+	});
+	adminUser.save();
+	const isAdmin = adminUser.generateAuthToken(true, true);
+	const regularUser = new User({
+		username : 'reg1UserName',
+		name     : {
+			firstName : 'testFirstName',
+			lastName  : 'testLastName',
+		},
+		email    : 'test1@test.com',
+		password : 'reg1Password',
+		roleId   : mongoose.Types.ObjectId(),
+	});
+	regularUser.save();
+	const isLogin = regularUser.generateAuthToken(true);
+
+
   afterAll(async () => {
 		await Document.deleteMany({});
 		await Category.deleteMany({});
@@ -22,15 +48,16 @@ describe('TEST FOR ROLE', () => {
       expect(res.status).toBe(401)
     });
     it('should return 403 if logged in user is not an admin', async () => {
-      const res = await request(app).post('/api/roles/').send({
+      const res = await request(app).post('/api/roles/').set('x-auth-token', isLogin).send({
         title: 'veteran'
       })
       expect(res.status).toBe(403)
     });
 
-    //check for login 
-    //check for ADMIN
-    //create role admin, 
+    //check for login 401
+    //check for ADMIN 403
+    //create role admin, 200
+    //role validation 400
   });
   
   //VIEW A CREATED ROLE : ALL USER
