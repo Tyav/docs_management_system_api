a31b677b6664d47d5a5b1133c5b93a45d09c56de
Tyav <tyav2greenz@gmail.com>
2019-04-16 11:56:21 +0100

    passed test for category creation data validation

--- api/v1/routes/category.js
+++ api/v1/routes/category.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Role } from '../../../server/model/role';
-import { validateRole } from '../../../server/validations/role';
+import { validateCate } from '../../../server/validations/category';
 import adminAuth from '../utils/admin';
 import tokenAuth from '../utils/auth';
 import loginAuth from '../utils/isLogin';
@@ -10,10 +10,11 @@ import { authId } from '../utils/validateId';
 const router = express.Router();
 
 router.post('/',[tokenAuth, adminAuth], (req, res)=>{
-
     //user should be logged in
     //user must be an admin,
     //valdation of category, return 400 if error
+    const {error} = validateCate(req.body)
+  if (error) return res.status(404).send({Error: 404, message: 'Invalid title format'})
     //create category 200
     //if duplication error return 400(dulicate error)
     res.status(201).send()
